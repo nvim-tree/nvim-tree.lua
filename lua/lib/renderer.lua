859be0bb76b87f7f7fdeb7da7031d19ce612bb5c
Kristijan Husak <husakkristijan@gmail.com>
2020-08-03 20:51:01 +0200

    Merge root folder and parent navigator into one.

--- lua/lib/renderer.lua
+++ lua/lib/renderer.lua
@@ -164,16 +164,10 @@ local root_folder_modifier = vim.g.lua_tree_root_folder_modifier or ':~'
 
 local function update_draw_data(tree, depth, markers)
   if tree.cwd and tree.cwd ~= '/' then
-    table.insert(lines, ".. (up a dir)")
-    table.insert(hl, {'LuaTreeFolderName', index, 0, 13})
-    index = 1
-  end
-
-  if tree.cwd then
-    local root_name = vim.fn.fnamemodify(tree.cwd, root_folder_modifier)
+    local root_name = vim.fn.fnamemodify(tree.cwd, root_folder_modifier):gsub('/$', '').."/.."
     table.insert(lines, root_name)
     table.insert(hl, {'LuaTreeRootFolder', index, 0, string.len(root_name)})
-    index = tree.cwd ~= '/' and 2 or 1
+    index = 1
   end
 
   for idx, node in ipairs(tree.entries) do
