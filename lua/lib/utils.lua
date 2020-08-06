62846b1e3111fd86c3afb8d8b178523254a1579c
Kristijan Husak <husakkristijan@gmail.com>
2020-08-06 11:48:00 +0200

    Print warning when git state is not recognized.

--- lua/lib/utils.lua
+++ lua/lib/utils.lua
@@ -1,7 +1,14 @@
 local M = {}
+local api = vim.api
 
 function M.path_to_matching_str(path)
   return path:gsub('(%-)', '(%%-)'):gsub('(%.)', '(%%.)'):gsub('(%_)', '(%%_)')
+end
+
+function M.echo_warning(msg)
+  api.nvim_command('echohl WarningMsg')
+  api.nvim_command("echom '[LuaTree] "..msg:gsub("'", "''").."'")
+  api.nvim_command('echohl None')
 end
 
 return M
