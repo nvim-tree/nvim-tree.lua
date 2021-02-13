bf89e1302498c50ef20b72b25f137a3bb8b792e7
dorgnarg <demoor12@asu.edu>
2021-02-12 20:57:30 -0700

    Resolve

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -128,7 +128,9 @@ function M.on_enter()
   if is_dir then
     api.nvim_command('cd '..bufname)
   end
-  local should_open = vim.g.nvim_tree_auto_open == 1 and ((is_dir and (hijack_netrw == 1 or disable_netrw == 1)) or bufname == '')
+  local should_open = vim.g.nvim_tree_auto_open == 1 and
+    ((is_dir and (hijack_netrw == 1 or disable_netrw == 1)) or bufname == '') and
+    not vim.tbl.contains(ft_ignore, buftype)
   colors.setup()
   lib.init(should_open, should_open)
 end
