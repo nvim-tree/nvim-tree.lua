a01a33f9a8aea3eb19eb76601131c01e23d8e034
kiyan <yazdani.kiyan@protonmail.com>
2021-07-01 10:35:01 +0200

    fix(help ui): adapt to new bindings and fix code lint/format

--- lua/nvim-tree/renderer.lua
+++ lua/nvim-tree/renderer.lua
@@ -370,33 +370,43 @@ local M = {}
 
 function M.draw_help()
   local help_lines = {'HELP'}
-  local help_hl = {{'NvimTreeRootFolder', 0, 0, string.len('HELP')}}
+  local help_hl = {{'NvimTreeRootFolder', 0, 0, #help_lines[1]}}
   local bindings = view.View.bindings
   local processed = {}
-  for i, v in pairs(bindings) do
-    if v:sub(1,35) == view.nvim_tree_callback('test'):sub(1,35) then
-      v = v:match("'[^']+'[^']*$")
-      v = v:match("'[^']+'")
-      table.insert(processed,{i,v,true})
+  for _, b in pairs(bindings) do
+    local cb = b.cb
+    local key = b.key
+    if cb:sub(1,35) == view.nvim_tree_callback('test'):sub(1,35) then
+      cb = cb:match("'[^']+'[^']*$")
+      cb = cb:match("'[^']+'")
+      table.insert(processed, {key, cb, true})
     else
-      v = '"' .. v .. '"'
-      table.insert(processed,{i,v,false})
+      cb = '"' .. cb .. '"'
+      table.insert(processed, {key, cb, false})
     end
   end
-  table.sort(processed,function(a,b)
-    return (a[3]==b[3] and (a[2]<b[2] or (a[2]==b[2] and #a[1]<#b[1]))) or (a[3] and not b[3])
+  table.sort(processed, function(a,b)
+    return (a[3] == b[3]
+      and (a[2] < b[2] or (a[2] == b[2] and #a[1] < #b[1])))
+      or (a[3] and not b[3])
   end)
-  local i, v, builtin
-  for num, val in pairs(processed) do
-    i = val[1]
-    v = val[2]
-    builtin = val[3]
-    local bind_string = string.format("%6s : %s",i,v)
-    table.insert(help_lines,bind_string)
-    local hl_len = math.max(6,#i)+2
-    table.insert(help_hl,{'NvimTreeFolderName', num, 0, hl_len})
-    if not builtin then
-      table.insert(help_hl,{'NvimTreeFileRenamed', num, hl_len, -1})
+
+  local num = 0
+  for _, val in pairs(processed) do
+    local keys = type(val[1]) == "string" and {val[1]} or val[1]
+    local map_name = val[2]
+    local builtin = val[3]
+    for _, key in pairs(keys) do
+      num = num + 1
+      local bind_string = string.format("%6s : %s", key, map_name)
+      table.insert(help_lines, bind_string)
+
+      local hl_len = math.max(6, string.len(key)) + 2
+      table.insert(help_hl, {'NvimTreeFolderName', num, 0, hl_len})
+
+      if not builtin then
+        table.insert(help_hl, {'NvimTreeFileRenamed', num, hl_len, -1})
+      end
     end
   end
   return help_lines, help_hl
