56189358c58ed2464c6cb54420f2655b2c1f230c
xeluxee <>
2021-07-31 18:19:20 +0200

    Fix Cygwin and MinGW integration + small refactoring

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -94,28 +94,28 @@ local keypress_funcs = {
     if vim.fn.has('win16') == 1 or vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
       system_command = 'start "" '
     elseif vim.fn.has('win32unix') == 1 then
-      system_command = 'cygstart '
+      if vim.fn.stridx(vim.fn.system('uname'), 'CYGWIN') ~= -1 then
+        system_command = 'cygstart '
+      else
+        system_command = 'start "" '
+      end
     elseif vim.fn.has('mac') == 1 or vim.fn.has('macunix') == 1 then
       system_command = 'open '
     elseif vim.fn.has('unix') == 1 then
       system_command = 'xdg-open '
-    end
-
-    if system_command == "" then
+    else
       vim.cmd('echohl ErrorMsg')
       vim.cmd('echomsg "NvimTree system_open: cannot open file with system application. Unsupported platform."')
       vim.cmd('echohl None')
       return
     end
 
-    local file_path = '"' .. vim.fn.substitute(node.absolute_path, '"', '\\\\"', 'g') .. '"'
-    local command_output = vim.fn.system(system_command .. file_path)
+    local command_output = vim.fn.system(system_command .. '"' .. vim.fn.substitute(node.absolute_path, '"', '\\\\"', 'g') .. '"')
 
     if vim.v.shell_error ~= 0 then
       vim.cmd('echohl ErrorMsg')
       vim.cmd(string.format('echomsg "NvimTree system_open: return code %d."', vim.v.shell_error))
-      command_output = vim.fn.substitute(command_output, '\n', '" | echomsg "', 'g')
-      vim.cmd('echomsg "' .. command_output .. '"')
+      vim.cmd('echomsg "' .. vim.fn.substitute(command_output, '\n', '" | echomsg "', 'g') .. '"')
       vim.cmd('echohl None')
     end
   end,
