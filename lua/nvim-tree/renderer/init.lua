08b351ac3a550dd3c6226002fd8d1b32cf07cc3a
kiyan <yazdani.kiyan@protonmail.com>
2021-09-27 19:43:23 +0200

    fix: renderer global options should be loaded on each reload

--- lua/nvim-tree/renderer/init.lua
+++ lua/nvim-tree/renderer/init.lua
@@ -249,25 +249,24 @@ local picture = {
   gif = true,
 }
 
-local special = vim.g.nvim_tree_special_files or {
-  ["Cargo.toml"] = true,
-  Makefile = true,
-  ["README.md"] = true,
-  ["readme.md"] = true,
-}
-
-local root_folder_modifier = vim.g.nvim_tree_root_folder_modifier or ':~'
-local hide_root_folder = vim.g.nvim_tree_hide_root_folder or 0;
-
 local function update_draw_data(tree, depth, markers)
-  if tree.cwd and tree.cwd ~= '/' and hide_root_folder ~= 1 then
-		local root_name = utils.path_join({
-			utils.path_remove_trailing(vim.fn.fnamemodify(tree.cwd, root_folder_modifier)),
-			".."
-		})
-		table.insert(lines, root_name)
-		table.insert(hl, {'NvimTreeRootFolder', index, 0, string.len(root_name)})
-		index = 1
+  local root_folder_modifier = vim.g.nvim_tree_root_folder_modifier or ':~'
+  local special = vim.g.nvim_tree_special_files or {
+    ["Cargo.toml"] = true,
+    Makefile = true,
+    ["README.md"] = true,
+    ["readme.md"] = true,
+  }
+
+  local hide_root_folder = view.View.hide_root_folder
+  if tree.cwd and tree.cwd ~= '/' and not hide_root_folder then
+    local root_name = utils.path_join({
+      utils.path_remove_trailing(vim.fn.fnamemodify(tree.cwd, root_folder_modifier)),
+      ".."
+    })
+    table.insert(lines, root_name)
+    table.insert(hl, {'NvimTreeRootFolder', index, 0, string.len(root_name)})
+    index = 1
   end
 
   for idx, node in ipairs(tree.entries) do
