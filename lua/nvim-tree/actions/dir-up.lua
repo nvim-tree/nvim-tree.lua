b91572da9e53a6cda7b07e281a5ed71629589b2c
Steve Vermeulen <sfvermeulen@gmail.com>
2022-02-09 22:17:41 +0000

    Fixed issue where user would have to trigger dir-up action multiple times to get it to work

--- lua/nvim-tree/actions/dir-up.lua
+++ lua/nvim-tree/actions/dir-up.lua
@@ -1,10 +1,12 @@
+local utils = require'nvim-tree.utils'
+
 local M = {}
 
 function M.fn(node)
   if not node or node.name == ".." then
     return require'nvim-tree.actions.change-dir'.fn('..')
   else
-    local newdir = vim.fn.fnamemodify(TreeExplorer.cwd, ':h')
+    local newdir = vim.fn.fnamemodify(utils.path_remove_trailing(TreeExplorer.cwd), ':h')
     require'nvim-tree.actions.change-dir'.fn(newdir)
     return require"nvim-tree.actions.find-file".fn(node.absolute_path)
   end
