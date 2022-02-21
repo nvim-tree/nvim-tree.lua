34a8de607c4cba59b86389e30109502ef3e0ef6d
Andreas Bissinger <be-ndee@users.noreply.github.com>
2022-02-21 21:46:53 +0100

    update code style

--- lua/nvim-tree/actions/search-node.lua
+++ lua/nvim-tree/actions/search-node.lua
@@ -10,28 +10,22 @@ function M.fn()
   local input_path = vim.fn.input("Search node: ", "", "file")
   utils.clear_prompt()
 
-  absolute_input_path = TreeExplorer.cwd .. utils.path_separator .. input_path
+  local absolute_input_path = TreeExplorer.cwd .. utils.path_separator .. input_path
 
   local function search_node(nodes)
     for _, node in ipairs(nodes) do
-      matches = utils.str_find(absolute_input_path, node.absolute_path .. utils.path_separator)
-      if matches then
-        if node.nodes then
-          if not node.open then
-            node.open = true
-            TreeExplorer:expand(node)
-            renderer.draw()
-          end
-          search_node(node.nodes)
+      local matches = utils.str_find(absolute_input_path, node.absolute_path)
+      if matches and node.nodes then
+        if not node.open then
+          node.open = true
+          TreeExplorer:expand(node)
         end
+        search_node(node.nodes)
       end
     end
   end
 
   node = search_node(TreeExplorer.nodes)
-end
-
-function M.setup(options)
 end
 
 return M
