5bbeac4eac2e574077c4edaf2be430fc73e0141c
Andreas Bissinger <mail@bissinger-andreas.de>
2022-03-01 07:58:07 +0100

    remove comments

--- lua/nvim-tree/actions/run-shell-command.lua
+++ lua/nvim-tree/actions/run-shell-command.lua
@@ -3,13 +3,11 @@ local utils = require"nvim-tree.utils"
 local M = {}
 
 function M.fn(node)
-  -- determine directory of node
   local path = node.absolute_path
   if not node.nodes then
     path = string.sub(path, 1, string.len(path) - string.len(node.name) - 1)
   end
 
-  -- ask for command
   local shell_command = vim.fn.input("Command: ", "", "shellcmd")
   utils.clear_prompt()
 
@@ -18,17 +16,14 @@ function M.fn(node)
     return
   end
 
-  -- print which path and command
   print("Path: " .. path .. "\n")
   print("Command: " .. shell_command .. "\n\n\n")
 
-  -- run command
   local file = io.popen("cd " .. path .. " && " .. shell_command)
   file:flush()
   local output = file:read("*all")
   file:close()
 
-  -- print output
   print(output)
 end
 
