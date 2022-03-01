38cc6305b4e3162a95ca328da36a65254b1593dd
kiyan <yazdani.kiyan@protonmail.com>
2022-03-01 21:11:11 +0100

    fix: forgot resize_window option in open-file setup

--- lua/nvim-tree/actions/open-file.lua
+++ lua/nvim-tree/actions/open-file.lua
@@ -226,6 +226,7 @@ end
 
 function M.setup(opts)
   M.quit_on_open = opts.actions.open_file.quit_on_open
+  M.resize_window = opts.actions.open_file.resize_window
   if opts.actions.open_file.window_picker.chars then
     opts.actions.open_file.window_picker.chars = tostring(opts.actions.open_file.window_picker.chars):upper()
   end
