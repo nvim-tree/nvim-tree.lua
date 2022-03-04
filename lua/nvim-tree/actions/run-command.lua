e68f77be33b58e63ba878e071ff4491845ec023f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-03-04 10:06:15 +0100

    fix(run-command): handle current directory node
    Handle the node representing the current directory (the topmost node in
    the nvim-tree window). That node does not have the `absolute_path` set.
    Use `TreeExplorer.cwd` instead, similar to the logic in `change-dir`.

--- lua/nvim-tree/actions/run-command.lua
+++ lua/nvim-tree/actions/run-command.lua
@@ -1,8 +1,21 @@
+local utils = require("nvim-tree.utils")
+
 local M = {}
 
+---Retrieves the absolute path to the node.
+---Safely handles the node representing the current directory
+---(the topmost node in the nvim-tree window)
+local function get_node_path(node)
+	if node.name == ".." then
+		return utils.path_remove_trailing(TreeExplorer.cwd)
+	else
+		return node.absolute_path
+	end
+end
+
 function M.run_file_command(node)
-  vim.api.nvim_input(": " .. node.absolute_path .. "<Home>")
+	local node_path = get_node_path(node)
+	vim.api.nvim_input(": " .. node_path .. "<Home>")
 end
 
 return M
-
