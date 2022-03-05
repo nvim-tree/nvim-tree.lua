88080447d956be66192f49767f3cd703dfc77abd
Alexander Courtis <alex@courtis.org>
2022-03-05 15:44:52 +1100

    #1050 script to update DEFAULT_OPTS in doc etc.

--- update-default-opts.sh
+++ update-default-opts.sh
@@ -14,21 +14,18 @@ sed -e "s/^  /      /" /tmp/DEFAULT_OPTS.2.lua > /tmp/DEFAULT_OPTS.6.lua
 sed -e "s/^  /          /" /tmp/DEFAULT_OPTS.2.lua > /tmp/DEFAULT_OPTS.10.lua
 
 # README.md indented at 2
-sed -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.2.lua
-        }; /${end}/p; d }" README.md > /tmp/README.md
-mv /tmp/README.md README.md
+sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.2.lua
+           }; /${end}/p; d }" README.md
 
 # help, indented at 6
 begin="^    require'nvim-tree'.setup {        $"
 end="^    }        $"
-sed -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.6.lua
-        s/  /aoeu/g; }; /${end}/p; d }" doc/nvim-tree-lua.txt > /tmp/nvim-tree-lua.txt
-mv /tmp/nvim-tree-lua.txt doc/nvim-tree-lua.txt
+sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.6.lua
+           }; /${end}/p; d }" doc/nvim-tree-lua.txt
 
 # bug_report.yml indented at 10
 begin="^        require'nvim-tree'.setup {        $"
 end="^        }        $"
-sed -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.10.lua
-        }; /${end}/p; d }" .github/ISSUE_TEMPLATE/bug_report.yml > /tmp/bug_report.yml
-mv /tmp/bug_report.yml .github/ISSUE_TEMPLATE/bug_report.yml
+sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.10.lua
+           }; /${end}/p; d }" .github/ISSUE_TEMPLATE/bug_report.yml
 
