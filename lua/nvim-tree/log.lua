a65078e3ae885a31bf3b3c78de59c8a5849ede66
Alexander Courtis <alex@courtis.org>
2022-03-13 00:43:33 +1100

    #1060 use one log file with the option to truncate (#1075)

--- lua/nvim-tree/log.lua
+++ lua/nvim-tree/log.lua
@@ -19,20 +19,23 @@ function M.raw(typ, fmt, ...)
   io.close(file)
 end
 
--- Write to log file
+-- Write to log file via M.raw
 -- time and typ are prefixed and a trailing newline is added
 function M.line(typ, fmt, ...)
   if not M.path or not M.config.types[typ] and not M.config.types.all then
     return
   end
 
-  M.raw(typ, string.format("[%s] [%s] %s\n", os.date "%H:%M:%S", typ, fmt), ...)
+  M.raw(typ, string.format("[%s] [%s] %s\n", os.date "%Y:%m:%d %H:%M:%S", typ, fmt), ...)
 end
 
 function M.setup(opts)
   M.config = opts.log
   if M.config and M.config.enable and M.config.types then
-    M.path = string.format("%s/nvim-tree-%s-%s.log", vim.fn.stdpath "cache", os.date "%H:%M:%S", vim.env.USER)
+    M.path = string.format("%s/nvim-tree.log", vim.fn.stdpath "cache", os.date "%H:%M:%S", vim.env.USER)
+    if M.config.truncate then
+      os.remove(M.path)
+    end
     print("nvim-tree.lua logging to " .. M.path)
   end
 end
