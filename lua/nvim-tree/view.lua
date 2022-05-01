1964af2507418e9e69211c66a9927cb878c64658
Alexander Courtis <alex@courtis.org>
2022-05-01 17:01:14 +1000

    Revert "fix: replace vertsplit highlights (#1142) (#1219)"
    This reverts commit 86d573dad77ed453be479199c3fd5bd3c8c9095b.

--- lua/nvim-tree/view.lua
+++ lua/nvim-tree/view.lua
@@ -27,7 +27,7 @@ M.View = {
       "EndOfBuffer:NvimTreeEndOfBuffer",
       "Normal:NvimTreeNormal",
       "CursorLine:NvimTreeCursorLine",
-      "WinSeparator:NvimTreeWinSeparator",
+      "VertSplit:NvimTreeVertSplit",
       "StatusLine:NvimTreeStatusLine",
       "StatusLineNC:NvimTreeStatuslineNC",
       "SignColumn:NvimTreeSignColumn",
