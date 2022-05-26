8d374ef24b1073781c66a941ef9c7d94ca290cf4
bstaint <bstaint@gmail.com>
2022-05-26 21:18:06 +0800

    fix stylua check

--- lua/nvim-tree/git/utils.lua
+++ lua/nvim-tree/git/utils.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local has_cygpath = vim.fn.executable('cygpath') == 1
+local has_cygpath = vim.fn.executable "cygpath" == 1
 
 function M.get_toplevel(cwd)
   local cmd = "git -C " .. vim.fn.shellescape(cwd) .. " rev-parse --show-toplevel"
@@ -14,7 +14,7 @@ function M.get_toplevel(cwd)
   if vim.fn.has "win32" == 1 then
     -- msys2 git support
     if has_cygpath then
-        toplevel = vim.fn.system("cygpath -w " .. vim.fn.shellescape(toplevel))
+      toplevel = vim.fn.system("cygpath -w " .. vim.fn.shellescape(toplevel))
     end
     toplevel = toplevel:gsub("/", "\\")
   end
