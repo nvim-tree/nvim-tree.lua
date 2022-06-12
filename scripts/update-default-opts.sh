e46b7571de4935fa06aca3e6bc487fe17837f5c1
Alexander Courtis <alex@courtis.org>
2022-06-12 15:21:06 +1000

    docs: fix opts scraping; try and make more macos compatible

--- scripts/update-default-opts.sh
+++ scripts/update-default-opts.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # run after changing nvim-tree.lua DEFAULT_OPTS or nvim-tree/actions/init.lua M.mappings
-# scrapes and updates README.md, nvim-tree-lua.txt
+# scrapes and updates nvim-tree-lua.txt
 # run from repositry root: scripts/update-default-opts.sh
 
 
 begin="BEGIN_DEFAULT_OPTS"
 end="END_DEFAULT_OPTS"
 
+# scrape DEFAULT_OPTS, indented at 2
+sed -n -e "/${begin}/,/${end}/{ /${begin}/d; /${end}/d; p; }" lua/nvim-tree.lua > /tmp/DEFAULT_OPTS.2.lua
+
 # indent some more
 sed -e "s/^  /      /" /tmp/DEFAULT_OPTS.2.lua > /tmp/DEFAULT_OPTS.6.lua
 
 # help, indented at 6
 sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.6.lua
-           }; /${end}/p; d }" doc/nvim-tree-lua.txt
+           }; /${end}/p; d; }" doc/nvim-tree-lua.txt
 
 
 begin="BEGIN_DEFAULT_MAPPINGS"
@@ -21,7 +24,7 @@ end="END_DEFAULT_MAPPINGS"
 
 # generate various DEFAULT_MAPPINGS
 sed -n -e "/${begin}/,/${end}/{ /${begin}/d; /${end}/d; p; }" lua/nvim-tree/actions/init.lua > /tmp/DEFAULT_MAPPINGS.M.lua
-lua <(cat /tmp/DEFAULT_MAPPINGS.M.lua scripts/generate_default_mappings.lua)
+cat /tmp/DEFAULT_MAPPINGS.M.lua scripts/generate_default_mappings.lua | lua
 
 # help
 sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_MAPPINGS.lua
