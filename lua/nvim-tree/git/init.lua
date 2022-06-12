2fa09d4fcb9ee14a422de9013acd2178dfd3d9d3
kiyan <yazdani.kiyan@protonmail.com>
2022-06-12 12:55:25 +0200

    chore(setup): make setup idempotent

--- lua/nvim-tree/git/init.lua
+++ lua/nvim-tree/git/init.lua
@@ -134,6 +134,11 @@ function M.load_project_status(cwd)
   return M.projects[project_root]
 end
 
+function M.purge_state()
+  M.projects = {}
+  M.cwd_to_project_root = {}
+end
+
 function M.setup(opts)
   M.config = opts.git
   M.config.watcher = opts.filesystem_watchers
