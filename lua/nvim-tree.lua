7874c5322cd51a6942f4cad170b7696e628283ee
beauwilliams <beau.williams@icloud.com>
2022-07-27 00:26:27 +1000

    fix: issue with telescope being opened when opening a directory in vim

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -418,6 +418,7 @@ local function setup_autocommands(opts)
   if opts.quit_if_last_window then
   vim.api.nvim_create_autocmd("BufEnter", {
 	group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  pattern = "NvimTree_*",
 	callback = function()
 		local layout = vim.api.nvim_call_function("winlayout", {})
 		if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
