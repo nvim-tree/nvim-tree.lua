b4e712ee08739b17250a231a6b187b192a145cdf
Beau <beau.williams@icloud.com>
2022-07-26 23:29:03 +1000

    feat: gracefully exit if there are unsaved buffers

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -420,7 +420,7 @@ local function setup_autocommands(opts)
 	group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
 	callback = function()
 		local layout = vim.api.nvim_call_function("winlayout", {})
-		if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("quit") end
+		if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
 	end
 })
   end
