f9a0c3cc1c5c423cb2d90ef6459dad0f2abc08e2
Alexander Courtis <alex@courtis.org>
2022-11-07 13:03:49 +1100

    fix(#1723): find-file refresh containing folder prior to searching

--- lua/nvim-tree/actions/finders/find-file.lua
+++ lua/nvim-tree/actions/finders/find-file.lua
@@ -3,6 +3,7 @@ local view = require "nvim-tree.view"
 local utils = require "nvim-tree.utils"
 local renderer = require "nvim-tree.renderer"
 local core = require "nvim-tree.core"
+local watch = require "nvim-tree.explorer.watch"
 local Iterator = require "nvim-tree.iterators.node-iterator"
 
 local M = {}
@@ -27,6 +28,8 @@ function M.fn(fname)
   local line = core.get_nodes_starting_line()
 
   local absolute_paths_searched = {}
+
+  watch.refresh_path(vim.fn.fnamemodify(fname, ":h"))
 
   local found = Iterator.builder(core.get_explorer().nodes)
     :matcher(function(node)
