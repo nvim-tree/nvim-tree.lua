6f883d2f27a4768674b6ed44babf19919167b0ff
Alexander Courtis <alex@courtis.org>
2023-01-22 16:53:21 +1100

    doc: api.tree.*

--- lua/nvim-tree/api.lua
+++ lua/nvim-tree/api.lua
@@ -77,7 +77,6 @@ Api.tree.toggle_hidden_filter = require("nvim-tree.actions.tree-modifiers.toggle
 
 Api.tree.toggle_help = require("nvim-tree.actions.tree-modifiers.toggles").help
 
-
 Api.fs.create = inject_node(require("nvim-tree.actions.fs.create-file").fn)
 Api.fs.remove = inject_node(require("nvim-tree.actions.fs.remove-file").fn)
 Api.fs.trash = inject_node(require("nvim-tree.actions.fs.trash").fn)
