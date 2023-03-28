c1e0ce77ca542daa1f3aed6643dc1f377c3d72ca
Alexander Courtis <alex@courtis.org>
2023-03-28 11:10:01 +1100

    fix(#2088): update tree root when actions.change_dir disabled

--- lua/nvim-tree/actions/root/change-dir.lua
+++ lua/nvim-tree/actions/root/change-dir.lua
@@ -64,11 +64,12 @@ end
 
 M.force_dirchange = add_profiling_to(function(foldername, should_open_view)
   local valid_dir = vim.fn.isdirectory(foldername) == 1 -- prevent problems on non existing dirs
-  if should_change_dir() and valid_dir then
-    cd(M.options.global, foldername)
+  if valid_dir then
+    if should_change_dir() then
+      cd(M.options.global, foldername)
+    end
+    core.init(foldername)
   end
-
-  core.init(foldername)
 
   if should_open_view then
     require("nvim-tree.lib").open()
