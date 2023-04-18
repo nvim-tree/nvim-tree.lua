a774fa186c19945d2a594de80922da2cbb4a0139
Alexander Courtis <alex@courtis.org>
2023-04-18 16:51:57 +1000

    feat(api): add tree.is_visible, tree.is_tree_buf (#2150)
    * feat(api): add api.tree.is_visible
    
    * feat(api): add api.tree.is_tree_buf

--- lua/nvim-tree/api.lua
+++ lua/nvim-tree/api.lua
@@ -107,6 +107,14 @@ Api.tree.toggle_hidden_filter = wrap(require("nvim-tree.actions.tree-modifiers.t
 
 Api.tree.toggle_help = wrap(require("nvim-tree.help").toggle)
 
+Api.tree.is_tree_buf = wrap(require("nvim-tree.utils").is_nvim_tree_buf)
+
+---@class ApiTreeIsVisibleOpts
+---@field tabpage number|nil
+---@field any_tabpage boolean|nil default false
+
+Api.tree.is_visible = wrap(require("nvim-tree.view").is_visible)
+
 Api.fs.create = wrap_node(require("nvim-tree.actions.fs.create-file").fn)
 Api.fs.remove = wrap_node(require("nvim-tree.actions.fs.remove-file").fn)
 Api.fs.trash = wrap_node(require("nvim-tree.actions.fs.trash").fn)
