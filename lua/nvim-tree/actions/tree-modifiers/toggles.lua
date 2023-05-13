22ba98ffd3733b2e91803503f6ec307b317ec8f1
Azad <49314270+Akmadan23@users.noreply.github.com>
2023-05-13 15:16:11 +0200

    fix(#1785): retain focused node on filter toggle

--- lua/nvim-tree/actions/tree-modifiers/toggles.lua
+++ lua/nvim-tree/actions/tree-modifiers/toggles.lua
@@ -1,31 +1,52 @@
+local lib = require "nvim-tree.lib"
+local utils = require "nvim-tree.utils"
 local filters = require "nvim-tree.explorer.filters"
 local reloaders = require "nvim-tree.actions.reloaders.reloaders"
 
 local M = {}
 
+local function reload()
+  local node = lib.get_node_at_cursor()
+  reloaders.reload_explorer()
+  local visible_nodes = require("nvim-tree.core").get_explorer().nodes
+
+  while node do
+    local found_node, _ = utils.find_node(visible_nodes, function(node_)
+      return node_.absolute_path == node.absolute_path
+    end)
+
+    if found_node or node.parent == nil then
+      utils.focus_file(node.absolute_path)
+      break
+    end
+
+    node = node.parent
+  end
+end
+
 function M.custom()
   filters.config.filter_custom = not filters.config.filter_custom
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.git_ignored()
   filters.config.filter_git_ignored = not filters.config.filter_git_ignored
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.git_clean()
   filters.config.filter_git_clean = not filters.config.filter_git_clean
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.no_buffer()
   filters.config.filter_no_buffer = not filters.config.filter_no_buffer
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.dotfiles()
   filters.config.filter_dotfiles = not filters.config.filter_dotfiles
-  return reloaders.reload_explorer()
+  reload()
 end
 
 return M
