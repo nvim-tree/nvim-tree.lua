d5d6950a0d0cc2692dfce9a1251b42f4da69136f
Azad <49314270+Akmadan23@users.noreply.github.com>
2023-05-27 05:47:36 +0200

    fix(#1785): retain focused node on filter toggles (#2202)
    * feat(live-filter): focus selected node after clear
    
    * fix(#1785): retain focused node on filter toggle
    
    * fix(#1785): apply requested changes
    
    * fix(live-filter): focus last focused node when cleared on prompt
    
    * refactor: store last focused node in `view` module
    
    * refactor: store last focused node in view module
    
    ---------
    
    Co-authored-by: Alexander Courtis <alex@courtis.org>

--- lua/nvim-tree/actions/tree-modifiers/toggles.lua
+++ lua/nvim-tree/actions/tree-modifiers/toggles.lua
@@ -1,31 +1,57 @@
+local lib = require "nvim-tree.lib"
+local core = require "nvim-tree.core"
+local utils = require "nvim-tree.utils"
 local filters = require "nvim-tree.explorer.filters"
 local reloaders = require "nvim-tree.actions.reloaders.reloaders"
 
 local M = {}
 
+local function reload()
+  local node = lib.get_node_at_cursor()
+  reloaders.reload_explorer()
+  local explorer = core.get_explorer()
+
+  if explorer == nil then
+    return
+  end
+
+  while node do
+    local found_node, _ = utils.find_node(explorer.nodes, function(node_)
+      return node_.absolute_path == node.absolute_path
+    end)
+
+    if found_node or node.parent == nil then
+      utils.focus_file(node.absolute_path)
+      break
+    end
+
+    node = node.parent
+  end
+end
+
 function M.custom()
   filters.config.filter_custom = not filters.config.filter_custom
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.git_ignored()
   filters.config.filter_git_ignored = not filters.config.filter_git_ignored
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.git_clean()
   filters.config.filter_git_clean = not filters.config.filter_git_clean
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.no_buffer()
   filters.config.filter_no_buffer = not filters.config.filter_no_buffer
-  return reloaders.reload_explorer()
+  reload()
 end
 
 function M.dotfiles()
   filters.config.filter_dotfiles = not filters.config.filter_dotfiles
-  return reloaders.reload_explorer()
+  reload()
 end
 
 return M
