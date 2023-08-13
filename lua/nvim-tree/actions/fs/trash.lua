04ea7065e3daea38fbc18bb21a14d830abe94348
Alexander Courtis <alex@courtis.org>
2023-08-13 12:14:45 +1000

    fix: trash.cmd message

--- lua/nvim-tree/actions/fs/trash.lua
+++ lua/nvim-tree/actions/fs/trash.lua
@@ -31,7 +31,7 @@ function M.fn(node)
 
   local binary = M.config.trash.cmd:gsub(" .*$", "")
   if vim.fn.executable(binary) == 0 then
-    notify.warn(string.format("trash.cmd '%s' is not available.", M.config.trash.cmd))
+    notify.warn(string.format("trash.cmd '%s' is not an executable.", M.config.trash.cmd))
     return
   end
 
