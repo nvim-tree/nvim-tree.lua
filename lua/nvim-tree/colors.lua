2aaa41d384b92fffa507e9147d5ed04a78b9655c
Alexander Courtis <alex@courtis.org>
2023-10-15 17:33:41 +1100

    chore: normalise colours and enable cterm

--- lua/nvim-tree/colors.lua
+++ lua/nvim-tree/colors.lua
@@ -1,42 +1,82 @@
 local M = {}
 
--- nvim-tree default highlight group links
-local DEFAULT_LINKS = {
-  -- File Text
-  NvimTreeFolderName = "Directory",
-  NvimTreeEmptyFolderName = "Directory",
-  NvimTreeOpenedFolderName = "Directory",
-  NvimTreeSymlinkFolderName = "Directory",
+-- directly defined groups, please keep these to an absolute minimum
+local DEFAULT_DEFS = {
 
-  -- Folder Text
-  NvimTreeOpenedFileIcon = "NvimTreeOpenedFile",
-  NvimTreeOpenedFolderIcon = "NvimTreeFolderIcon",
-  NvimTreeClosedFolderIcon = "NvimTreeFolderIcon",
+  NvimTreeFolderIcon = "guifg=#8094b4 ctermfg=Blue",
+  NvimTreeWindowPicker = "guifg=#ededed guibg=#4493c8 gui=bold ctermfg=White ctermbg=Cyan",
+}
+
+-- nvim-tree default highlight group links, please attempt to keep in order with help
+local DEFAULT_LINKS = {
 
   -- Standard
   NvimTreeNormal = "Normal",
   NvimTreeNormalFloat = "NormalFloat",
   NvimTreeNormalNC = "NvimTreeNormal",
+
   NvimTreeLineNr = "LineNr",
   NvimTreeWinSeparator = "WinSeparator",
   NvimTreeEndOfBuffer = "EndOfBuffer",
   NvimTreePopup = "Normal",
   NvimTreeSignColumn = "NvimTreeNormal",
-  NvimTreeCursorLine = "CursorLine",
+
   NvimTreeCursorColumn = "CursorColumn",
+  NvimTreeCursorLine = "CursorLine",
   NvimTreeCursorLineNr = "CursorLineNr",
+
   NvimTreeStatusLine = "StatusLine",
   NvimTreeStatusLineNC = "StatusLineNC",
+
+  -- File Text
+  NvimTreeExecFile = "Constant",
+  NvimTreeImageFile = "PreProc",
+  NvimTreeModifiedFile = "Constant",
+  NvimTreeOpenedFile = "Constant",
+  NvimTreeSpecialFile = "PreProc",
+  NvimTreeSymlink = "Statement",
+
+  -- Folder Text
+  NvimTreeRootFolder = "PreProc",
+  NvimTreeFolderName = "Directory",
+  NvimTreeEmptyFolderName = "Directory",
+  NvimTreeOpenedFolderName = "Directory",
+  NvimTreeSymlinkFolderName = "Directory",
+
+  -- Icon
+  NvimTreeFileIcon = "NvimTreeNormal",
+  NvimTreeSymlinkIcon = "NvimTreeNormal",
+  NvimTreeOpenedFileIcon = "NvimTreeOpenedFile",
+  NvimTreeOpenedFolderIcon = "NvimTreeFolderIcon",
+  NvimTreeClosedFolderIcon = "NvimTreeFolderIcon",
+  NvimTreeFolderArrowClosed = "NvimTreeIndentMarker",
+  NvimTreeFolderArrowOpen = "NvimTreeIndentMarker",
+
+  -- Indent
+  NvimTreeIndentMarker = "NvimTreeFileIcon",
 
   -- Clipboard
   NvimTreeCutHL = "SpellBad",
   NvimTreeCopiedHL = "SpellRare",
 
+  -- Bookmark Icon
+  NvimTreeBookmark = "Constant",
+
   -- Bookmark Highlight
   NvimTreeBookmarkHL = "SpellLocal",
 
+  -- LiveFilter
+  NvimTreeLiveFilterPrefix = "PreProc",
+  NvimTreeLiveFilterValue = "ModeMsg",
+
   -- Git Icon
+  NvimTreeGitDeleted = "Statement",
+  NvimTreeGitDirty = "Statement",
   NvimTreeGitIgnored = "Comment",
+  NvimTreeGitMerge = "Constant",
+  NvimTreeGitNew = "PreProc",
+  NvimTreeGitRenamed = "PreProc",
+  NvimTreeGitStaged = "Constant",
 
   -- Git File Text
   NvimTreeFileDirty = "NvimTreeGitDirty",
@@ -91,71 +131,10 @@ local LEGACY_LINKS = {
   NvimTreeDiagnosticHintFolderHL = "NvimTreeLspDiagnosticsHintFolderText",
 }
 
-local function get_color_from_hl(hl_name, fallback)
-  local id = vim.api.nvim_get_hl_id_by_name(hl_name)
-  if not id then
-    return fallback
-  end
-
-  -- TODO this is unreachable as nvim_get_hl_id_by_name returns a new ID if not present
-  local foreground = vim.fn.synIDattr(vim.fn.synIDtrans(id), "fg")
-  if not foreground or foreground == "" then
-    return fallback
-  end
-
-  return foreground
-end
-
-local function get_colors()
-  return {
-    red = vim.g.terminal_color_1 or get_color_from_hl("Keyword", "Red"),
-    green = vim.g.terminal_color_2 or get_color_from_hl("Character", "Green"),
-    yellow = vim.g.terminal_color_3 or get_color_from_hl("PreProc", "Yellow"),
-    blue = vim.g.terminal_color_4 or get_color_from_hl("Include", "Blue"),
-    purple = vim.g.terminal_color_5 or get_color_from_hl("Define", "Purple"),
-    cyan = vim.g.terminal_color_6 or get_color_from_hl("Conditional", "Cyan"),
-    dark_red = vim.g.terminal_color_9 or get_color_from_hl("Keyword", "DarkRed"),
-    orange = vim.g.terminal_color_11 or get_color_from_hl("Number", "Orange"),
-  }
-end
-
-local function get_hl_groups()
-  local colors = get_colors()
-
-  return {
-    IndentMarker = { fg = "#8094b4" },
-    Symlink = { gui = "bold", fg = colors.cyan },
-    FolderIcon = { fg = "#8094b4" },
-    RootFolder = { fg = colors.purple },
-
-    ExecFile = { gui = "bold", fg = colors.green },
-    SpecialFile = { gui = "bold,underline", fg = colors.yellow },
-    ImageFile = { gui = "bold", fg = colors.purple },
-    OpenedFile = { gui = "bold", fg = colors.green },
-    ModifiedFile = { fg = colors.green },
-
-    GitDirty = { fg = colors.dark_red },
-    GitDeleted = { fg = colors.dark_red },
-    GitStaged = { fg = colors.green },
-    GitMerge = { fg = colors.orange },
-    GitRenamed = { fg = colors.purple },
-    GitNew = { fg = colors.yellow },
-
-    WindowPicker = { gui = "bold", fg = "#ededed", bg = "#4493c8" },
-    LiveFilterPrefix = { gui = "bold", fg = colors.purple },
-    LiveFilterValue = { gui = "bold", fg = "#fff" },
-
-    Bookmark = { fg = colors.green },
-  }
-end
-
 function M.setup()
-  local highlight_groups = get_hl_groups()
-  for k, d in pairs(highlight_groups) do
-    local gui = d.gui and " gui=" .. d.gui or ""
-    local fg = d.fg and " guifg=" .. d.fg or ""
-    local bg = d.bg and " guibg=" .. d.bg or ""
-    vim.api.nvim_command("hi def NvimTree" .. k .. gui .. fg .. bg)
+  -- non-linked
+  for k, d in pairs(DEFAULT_DEFS) do
+    vim.api.nvim_command("hi " .. k .. " " .. d)
   end
 
   -- hard link override when legacy only is present
