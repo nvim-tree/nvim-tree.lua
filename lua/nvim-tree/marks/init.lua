03ceffcf0995549e4ef8611cb12af8f8d8641005
Akmadan23 <azadahmadi@mailo.com>
2023-11-27 13:54:58 +0100

    Add `MinimalNode` class in `marks` module

--- lua/nvim-tree/marks/init.lua
+++ lua/nvim-tree/marks/init.lua
@@ -4,21 +4,24 @@ local NvimTreeMarks = {}
 
 local M = {}
 
----@param node Node
+---@class MinimalNode
+---@field absolute_path string
+
+---@param node Node|MinimalNode
 local function add_mark(node)
   NvimTreeMarks[node.absolute_path] = node
 
   renderer.draw()
 end
 
----@param node Node
+---@param node Node|MinimalNode
 local function remove_mark(node)
   NvimTreeMarks[node.absolute_path] = nil
 
   renderer.draw()
 end
 
----@param node Node
+---@param node Node|MinimalNode
 function M.toggle_mark(node)
   if node.absolute_path == nil then
     return
@@ -39,7 +42,7 @@ function M.clear_marks()
   renderer.draw()
 end
 
----@param node Node
+---@param node Node|MinimalNode
 ---@return table|nil
 function M.get_mark(node)
   return NvimTreeMarks[node.absolute_path]
