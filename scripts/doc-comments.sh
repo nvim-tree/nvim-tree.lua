2fed5e1010a591f81e9ceffd2054c469feb4e95e
Azad <49314270+Akmadan23@users.noreply.github.com>
2023-12-09 02:06:06 +0100

    ci: add style check for doc comments (#2575)
    Co-authored-by: Alexander Courtis <alex@courtis.org>

---
+++ scripts/doc-comments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+out=$(grep -nr "^--- @" lua)
+
+if [ "$out" ]; then
+	last_file=""
+	while read -r line; do
+		file="$(echo "$line" | cut -d: -f1)"
+		if [[ "$file" != "$last_file" ]]; then
+			echo "$file:" >&2
+			last_file="$file"
+		fi
+		echo "$line" | awk -F: '{ printf("  line %s: %s\n", $2, $3) }' >&2
+	done <<< "$out"
+	exit 1
+fi
