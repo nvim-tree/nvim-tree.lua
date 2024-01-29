199c7c0f18bc812b8a291f5e5bc3d6d50ad194c8
mohamed arish <arish1248@gmail.com>
2024-01-29 20:49:45 +0530

    Added creating of directories when renaming files

--- lua/nvim-tree/actions/fs/rename-file.lua
+++ lua/nvim-tree/actions/fs/rename-file.lua
@@ -9,6 +9,16 @@ local M = {
   config = {},
 }
 
+---@param iter function iterable
+---@return integer
+local function get_num_nodes(iter)
+  local i = 0
+  for _ in iter do
+    i = i + 1
+  end
+  return i
+end
+
 local ALLOWED_MODIFIERS = {
   [":p"] = true,
   [":p:h"] = true,
@@ -26,20 +36,48 @@ function M.rename(node, to)
   local notify_from = notify.render_path(node.absolute_path)
   local notify_to = notify.render_path(to)
 
-  if utils.file_exists(to) then
-    notify.warn(err_fmt(notify_from, notify_to, "file already exists"))
-    return
+  -- create a folder for each path element if the folder does not exist
+  local idx = 0
+  local path_to_create = ""
+  local is_last_path_file = not to:match(utils.path_remove_trailing(to))
+
+  local num_nodes = get_num_nodes(utils.path_split(utils.path_remove_trailing(to)))
+  local is_error = false
+  for path in utils.path_split(to) do
+    idx = idx + 1
+
+    local p = utils.path_remove_trailing(path)
+    if #path_to_create == 0 and vim.fn.has "win32" == 1 then
+      path_to_create = utils.path_join { p, path_to_create }
+    else
+      path_to_create = utils.path_join { path_to_create, p }
+    end
+
+    if is_last_path_file and idx == num_nodes then
+      events._dispatch_will_rename_node(node.absolute_path, to)
+      local success, err = vim.loop.fs_rename(node.absolute_path, to)
+
+      if not success then
+        notify.warn(err_fmt(notify_from, notify_to, err))
+        is_error = true
+        break
+      end
+    elseif not utils.file_exists(path_to_create) then
+      local success = vim.loop.fs_mkdir(path_to_create, 493)
+      if not success then
+        notify.error("Could not create folder " .. notify.render_path(path_to_create))
+        break
+      end
+      is_error = true
+      events._dispatch_folder_created(to)
+    end
   end
 
-  events._dispatch_will_rename_node(node.absolute_path, to)
-  local success, err = vim.loop.fs_rename(node.absolute_path, to)
-  if not success then
-    notify.warn(err_fmt(notify_from, notify_to, err))
-    return
+  if not is_error then
+    notify.info(string.format("%s -> %s", notify_from, notify_to))
+    utils.rename_loaded_buffers(node.absolute_path, to)
+    events._dispatch_node_renamed(node.absolute_path, to)
   end
-  notify.info(string.format("%s -> %s", notify_from, notify_to))
-  utils.rename_loaded_buffers(node.absolute_path, to)
-  events._dispatch_node_renamed(node.absolute_path, to)
 end
 
 ---@param default_modifier string|nil
@@ -62,7 +100,8 @@ function M.fn(default_modifier)
 
     -- support for only specific modifiers have been implemented
     if not ALLOWED_MODIFIERS[modifier] then
-      notify.warn("Modifier " .. vim.inspect(modifier) .. " is not in allowed list : " .. table.concat(ALLOWED_MODIFIERS, ","))
+      notify.warn("Modifier " ..
+      vim.inspect(modifier) .. " is not in allowed list : " .. table.concat(ALLOWED_MODIFIERS, ","))
       return
     end
 
