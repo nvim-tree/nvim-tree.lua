719626aed7b1a47aef6dbffbfa4e446b8a3ed63e
Yida Zhang <zyd200101@outlook.com>
2024-03-31 01:03:08 -0400

    fix format string

--- lua/nvim-tree/actions/fs/copy-paste.lua
+++ lua/nvim-tree/actions/fs/copy-paste.lua
@@ -290,7 +290,7 @@ local function copy_to_clipboard(content)
   local temp_buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_text(temp_buf, 0, 0, 0, 0, { content })
   vim.api.nvim_buf_call(temp_buf, function ()
-    vim.cmd(string.format('normal! "%dy$', reg))
+    vim.cmd(string.format('normal! "%sy$', reg))
   end)
   vim.api.nvim_buf_delete(temp_buf, {})
 
