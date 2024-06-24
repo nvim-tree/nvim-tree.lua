54c0874cd95bde7c59922dfaf712ae4c2970102f
Vladimir Levin <opaozhub@gmail.com>
2024-06-24 12:32:27 +0900

    feat(#2598): Implemented API `tree.resize`

--- lua/nvim-tree/actions/tree/init.lua
+++ lua/nvim-tree/actions/tree/init.lua
@@ -4,12 +4,14 @@ M.find_file = require "nvim-tree.actions.tree.find-file"
 M.modifiers = require "nvim-tree.actions.tree.modifiers"
 M.open = require "nvim-tree.actions.tree.open"
 M.toggle = require "nvim-tree.actions.tree.toggle"
+M.resize = require "nvim-tree.actions.tree.resize"
 
 function M.setup(opts)
   M.find_file.setup(opts)
   M.modifiers.setup(opts)
   M.open.setup(opts)
   M.toggle.setup(opts)
+  M.resize.setup(opts)
 end
 
 return M
