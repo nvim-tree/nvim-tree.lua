f7e545a3ed6e32dbd429b2c546e48a52dd0e8dff
Alexander Courtis <alex@courtis.org>
2024-09-15 13:09:41 +1000

    refactor(#2875): multi instance renderer

--- lua/nvim-tree/renderer/components/init.lua
+++ lua/nvim-tree/renderer/components/init.lua
@@ -1,13 +1,15 @@
 local M = {}
 
-M.padding = require "nvim-tree.renderer.components.padding"
+M.diagnostics = require "nvim-tree.renderer.components.diagnostics"
 M.full_name = require "nvim-tree.renderer.components.full-name"
 M.icons = require "nvim-tree.renderer.components.icons"
+M.padding = require "nvim-tree.renderer.components.padding"
 
 function M.setup(opts)
-  M.padding.setup(opts)
+  M.diagnostics.setup(opts)
   M.full_name.setup(opts)
   M.icons.setup(opts)
+  M.padding.setup(opts)
 end
 
 return M
