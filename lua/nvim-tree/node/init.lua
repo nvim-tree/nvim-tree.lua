d4242af40c98d339aa3857383b9c135a8ecae5f3
Alexander Courtis <alex@courtis.org>
2024-10-07 16:35:04 +1100

    temporarily move DirectoryNode methods into BaseNode for easier reviewing

--- lua/nvim-tree/node/init.lua
+++ lua/nvim-tree/node/init.lua
@@ -1,5 +1,6 @@
 local git = require("nvim-tree.git")
 
+---TODO remove all @cast, @as
 ---TODO remove all references to directory fields:
 ------@field has_children boolean
 ------@field group_next Node? -- If node is grouped, this points to the next child dir/link node
@@ -38,6 +39,11 @@ function BaseNode:new(o)
   return o
 end
 
+-- TODO temporary hack to allow DirectoryNode methods in this file, for easier reviewing
+---@class DirectoryNode
+local DirectoryNode = BaseNode:new()
+BaseNode.dn = DirectoryNode ---@diagnostic disable-line: inject-field
+
 function BaseNode:destroy()
   if self.watcher then
     self.watcher:destroy()
@@ -63,25 +69,8 @@ function BaseNode:is(T)
 end
 
 ---@return boolean
-function BaseNode:has_one_child_folder()
-  return false
-end
-
--- If node is grouped, return the last node in the group. Otherwise, return the given node.
----@return Node
-function BaseNode:last_group_node()
-  return self
-end
-
----Group empty folders
--- Recursively group nodes
----@return Node[]
-function BaseNode:group_empty_folders()
-  return {}
-end
-
----Ungroup empty folders
-function BaseNode:ungroup_empty_folders()
+function DirectoryNode:has_one_child_folder()
+  return #self.nodes == 1 and self.nodes[1].nodes and vim.loop.fs_access(self.nodes[1].absolute_path, "R") or false
 end
 
 ---Update the GitStatus of the node
@@ -94,8 +83,66 @@ end
 function BaseNode:get_git_status()
 end
 
----@param _ table projects
-function BaseNode:reload_node_status(_)
+---@return GitStatus|nil
+function DirectoryNode:get_git_status()
+  if not self.git_status or not self.explorer.opts.git.show_on_dirs then
+    return nil
+  end
+
+  local status = {}
+  if not self:last_group_node().open or self.explorer.opts.git.show_on_open_dirs then
+    -- dir is closed or we should show on open_dirs
+    if self.git_status.file ~= nil then
+      table.insert(status, self.git_status.file)
+    end
+    if self.git_status.dir ~= nil then
+      if self.git_status.dir.direct ~= nil then
+        for _, s in pairs(self.git_status.dir.direct) do
+          table.insert(status, s)
+        end
+      end
+      if self.git_status.dir.indirect ~= nil then
+        for _, s in pairs(self.git_status.dir.indirect) do
+          table.insert(status, s)
+        end
+      end
+    end
+  else
+    -- dir is open and we shouldn't show on open_dirs
+    if self.git_status.file ~= nil then
+      table.insert(status, self.git_status.file)
+    end
+    if self.git_status.dir ~= nil and self.git_status.dir.direct ~= nil then
+      local deleted = {
+        [" D"] = true,
+        ["D "] = true,
+        ["RD"] = true,
+        ["DD"] = true,
+      }
+      for _, s in pairs(self.git_status.dir.direct) do
+        if deleted[s] then
+          table.insert(status, s)
+        end
+      end
+    end
+  end
+  if #status == 0 then
+    return nil
+  else
+    return status
+  end
+end
+
+---@param projects table
+function DirectoryNode:reload_node_status(projects)
+  local toplevel = git.get_toplevel(self.absolute_path)
+  local status = projects[toplevel] or {}
+  for _, node in ipairs(self.nodes) do
+    node:update_git_status(self:is_git_ignored(), status)
+    if node.nodes and #node.nodes > 0 then
+      self:reload_node_status(projects)
+    end
+  end
 end
 
 ---@return boolean
@@ -114,6 +161,18 @@ function BaseNode:is_dotfile()
     return true
   end
   return false
+end
+
+-- If node is grouped, return the last node in the group. Otherwise, return the given node.
+---@return Node
+function DirectoryNode:last_group_node()
+  local node = self --[[@as BaseNode]]
+
+  while node.group_next do
+    node = node.group_next
+  end
+
+  return node
 end
 
 ---@param project table?
@@ -186,13 +245,39 @@ function BaseNode:get_all_nodes_in_group()
 end
 
 -- Toggle group empty folders
-function BaseNode:toggle_group_folders()
+function DirectoryNode:toggle_group_folders()
   local is_grouped = self.group_next ~= nil
 
   if is_grouped then
     self:ungroup_empty_folders()
   else
     self:group_empty_folders()
+  end
+end
+
+---Group empty folders
+-- Recursively group nodes
+---@return Node[]
+function DirectoryNode:group_empty_folders()
+  local is_root = not self.parent
+  local child_folder_only = self:has_one_child_folder() and self.nodes[1]
+  if self.explorer.opts.renderer.group_empty and not is_root and child_folder_only then
+    self.group_next = child_folder_only
+    local ns = child_folder_only:group_empty_folders()
+    self.nodes = ns or {}
+    return ns
+  end
+  return self.nodes
+end
+
+---Ungroup empty folders
+-- If a node is grouped, ungroup it: put node.group_next to the node.nodes and set node.group_next to nil
+function DirectoryNode:ungroup_empty_folders()
+  local cur = self --[[@as DirectoryNode]]
+  while cur and cur.group_next do
+    cur.nodes = { cur.group_next }
+    cur.group_next = nil
+    cur = cur.nodes[1] --[[@as DirectoryNode]]
   end
 end
 
@@ -208,6 +293,7 @@ function BaseNode:expand_or_collapse(toggle_group)
   end
 
   local head_node = self:get_parent_of_group()
+  ---@cast head_node DirectoryNode -- TODO move this to the class
   if toggle_group then
     head_node:toggle_group_folders()
   end
@@ -249,6 +335,36 @@ function BaseNode:clone()
   }
 
   return clone
+end
+
+--
+-- TODO temporary hack to allow DirectoryNode methods in this file, for easier reviewing
+--
+
+---@return boolean
+function BaseNode:has_one_child_folder()
+  return false
+end
+
+---@param _ table projects
+function BaseNode:reload_node_status(_)
+end
+
+-- If node is grouped, return the last node in the group. Otherwise, return the given node.
+---@return Node
+function BaseNode:last_group_node()
+  return self
+end
+
+---Group empty folders
+-- Recursively group nodes
+---@return Node[]
+function BaseNode:group_empty_folders()
+  return {}
+end
+
+---Ungroup empty folders
+function BaseNode:ungroup_empty_folders()
 end
 
 return BaseNode
