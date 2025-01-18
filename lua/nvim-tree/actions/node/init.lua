343b8a09e664d4e4104502fb0b180511d1f301b9
Gabriel Crispino <gabriel.crispino@hotmail.com>
2025-01-17 23:16:07 -0300

    refactor: merge delete-buffer and wipe-buffer into single buffer file

--- lua/nvim-tree/actions/node/init.lua
+++ lua/nvim-tree/actions/node/init.lua
@@ -4,15 +4,13 @@ M.file_popup = require("nvim-tree.actions.node.file-popup")
 M.open_file = require("nvim-tree.actions.node.open-file")
 M.run_command = require("nvim-tree.actions.node.run-command")
 M.system_open = require("nvim-tree.actions.node.system-open")
-M.delete_buffer = require("nvim-tree.actions.node.delete-buffer")
-M.wipe_buffer = require("nvim-tree.actions.node.wipe-buffer")
+M.buffer = require("nvim-tree.actions.node.buffer")
 
 function M.setup(opts)
   require("nvim-tree.actions.node.system-open").setup(opts)
   require("nvim-tree.actions.node.file-popup").setup(opts)
   require("nvim-tree.actions.node.open-file").setup(opts)
-  require("nvim-tree.actions.node.delete-buffer").setup(opts)
-  require("nvim-tree.actions.node.wipe-buffer").setup(opts)
+  require("nvim-tree.actions.node.buffer").setup(opts)
 end
 
 return M
