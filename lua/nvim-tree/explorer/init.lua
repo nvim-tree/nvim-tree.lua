30615ddecbcf3a5fdd5fd57dc4aca84c82459ffd
Alexander Courtis <alex@courtis.org>
2025-03-22 13:10:37 +1100

    chore: use luals for style check

--- lua/nvim-tree/explorer/init.lua
+++ lua/nvim-tree/explorer/init.lua
@@ -388,7 +388,7 @@ function Explorer:populate_children(handle, cwd, node, project, parent)
         })
         if child then
           table.insert(node.nodes, child)
-            nodes_by_path[child.absolute_path] = true
+          nodes_by_path[child.absolute_path] = true
           child:update_git_status(node_ignored, project)
         end
       elseif node.hidden_stats then
