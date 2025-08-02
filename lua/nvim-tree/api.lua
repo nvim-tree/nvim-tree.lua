06575bce2b0c17a61729e77393bee17936996075
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-08-02 14:42:16 +0200

    Rename field to match the api method name

--- lua/nvim-tree/api.lua
+++ lua/nvim-tree/api.lua
@@ -188,7 +188,7 @@ Api.tree.search_node = wrap(actions.finders.search_node.fn)
 Api.tree.collapse_all = wrap(actions.tree.modifiers.collapse.all)
 
 ---@class ApiTreeExpandAllOpts
----@field descend_until (fun(expansion_count: integer, node: Node): boolean)|nil
+---@field expand_until (fun(expansion_count: integer, node: Node): boolean)|nil
 
 Api.tree.expand_all = wrap_node(actions.tree.modifiers.expand.all)
 Api.tree.toggle_enable_filters = wrap_explorer_member("filters", "toggle")
