6a867ffc62c581a601dd40da2710bc7252f5e40d
Alexander Courtis <alex@courtis.org>
2026-01-05 12:15:07 +1100

    doc(#2934): add nvim_tree.Config meta classes, add nvim_tree.api. meta classes

--- lua/nvim-tree/_meta/api.lua
+++ lua/nvim-tree/_meta/api.lua
@@ -2,6 +2,59 @@
 error("Cannot require a meta file")
 
 --
+-- API Options
+--
+
+---@class nvim_tree.api.TreeOpenOpts
+---@field path? string root directory for the tree
+---@field current_window? boolean open the tree in the current window
+---@field winid? number open the tree in the specified winid, overrides current_window
+---@field find_file? boolean find the current buffer
+---@field update_root? boolean requires find_file, see |nvim-tree.update_focused_file.update_root|
+---@field focus? boolean focus the tree when opening, default true
+
+---@class nvim_tree.api.TreeToggleOpts
+---@field path? string root directory for the tree
+---@field current_window? boolean open the tree in the current window
+---@field winid? number open the tree in the specified |winid|, overrides current_window
+---@field find_file? boolean find the current buffer
+---@field update_root? boolean requires find_file, see |nvim-tree.update_focused_file.update_root|
+---@field focus? boolean focus the tree when opening, default true
+
+---@class nvim_tree.api.TreeResizeOpts
+---@field width? string|function|number|table new |nvim-tree.view.width| value
+---@field absolute? number set the width
+---@field relative? number relative width adjustment
+
+---@class nvim_tree.api.TreeFindFileOpts
+---@field buf? string|number absolute/relative path OR bufnr to find
+---@field open? boolean open the tree if necessary
+---@field current_window? boolean requires open, open in the current window
+---@field winid? number open the tree in the specified |winid|, overrides current_window
+---@field update_root? boolean see |nvim-tree.update_focused_file.update_root|
+---@field focus? boolean focus the tree
+
+---@class nvim_tree.api.CollapseOpts
+---@field keep_buffers? boolean do not collapse nodes with open buffers
+
+---@class nvim_tree.api.TreeExpandOpts
+---@field expand_until? (fun(expansion_count: integer, node: Node): boolean) Return true if node should be expanded. expansion_count is the total number of folders expanded.
+
+---@class nvim_tree.api.TreeIsVisibleOpts
+---@field tabpage? number as per |nvim_get_current_tabpage()|
+---@field any_tabpage? boolean visible on any tab, default false
+
+---@class nvim_tree.api.TreeWinIdOpts
+---@field tabpage? number tabpage, 0 or nil for current, default nil
+
+---@class nvim_tree.api.NodeEditOpts
+---@field quit_on_open? boolean quits the tree when opening the file
+---@field focus? boolean keep focus in the tree when opening the file
+
+---@class nvim_tree.api.NodeBufferOpts
+---@field force? boolean delete/wipe even if buffer is modified, default false
+
+--
 -- Nodes
 --
 
