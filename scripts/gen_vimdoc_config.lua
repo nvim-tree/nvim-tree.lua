d14385f5386416d25bf77ee964e6ac6485db305a
Alexander Courtis <alex@courtis.org>
2026-01-05 13:58:07 +1100

    doc(#2934): add nvim_tree.api classes to help

--- scripts/gen_vimdoc_config.lua
+++ scripts/gen_vimdoc_config.lua
@@ -1,36 +1,61 @@
 ---@diagnostic disable: undefined-doc-name
 
+-- module name is derived as the file name with the first letter capitalised
+local modules = {
+  Api = {
+    order = 2,
+    helptag = "nvim-tree-api",
+    title = "Lua module: nvim_tree.api",
+    path = "lua/nvim-tree/_meta/api.lua",
+  },
+  Config = {
+    order = 1,
+    helptag = "nvim-tree-module",
+    title = "Lua module: nvim_tree",
+    path = "lua/nvim-tree/_meta/config.lua",
+  },
+  Api_decorator = {
+    order = 3,
+    helptag = "nvim-tree-api-decorator",
+    title = "Lua module: nvim_tree.api.decorator",
+    path = "lua/nvim-tree/_meta/api_decorator.lua",
+  },
+}
+
 --- @type table<string,nvim.gen_vimdoc.Config>
 local config = {
   decorator = {
     filename = "nvim-tree-lua.txt",
-    -- filename = "decorator.txt",
-    section_order = {
-      "config.lua",
-      "api_decorator.lua",
-    },
-    files = {
-      -- module is derived soley from the file name, first letter capitalised
-      "lua/nvim-tree/_meta/api_decorator.lua",
-      "lua/nvim-tree/_meta/config.lua",
-    },
+
+    -- file name sets order
+    section_order = (function()
+      local ret = {}
+      for _, c in pairs(modules) do
+        ret[c.order] = vim.fn.fnamemodify(c.path, ":t")
+      end
+      return ret
+    end)(),
+
+    -- full path, will be ordered by section_order
+    files = (function()
+      local ret = {}
+      for _, c in pairs(modules) do
+        table.insert(ret, c.path)
+      end
+      return ret
+    end)(),
+
+    -- section title
     section_fmt = function(name)
-      if name == "Config" then
-        return "Lua module: nvim_tree.Config"
-      elseif name == "Api_decorator" then
-        return "Lua module: nvim_tree.api.decorator"
-      end
-      error(string.format("unknown module %s passed to section_fmt", name))
+      return modules[name] and modules[name].title or error(string.format("unknown module %s passed to section", name))
     end,
+
+    -- section's help tag
     helptag_fmt = function(name)
-      -- used to locate the first section only, others will be rendered after
-      if name == "Config" then
-        return "nvim-tree-config"
-      elseif name == "Api_decorator" then
-        return "nvim-tree-api-decorator"
-      end
-      error(string.format("unknown module %s passed to helptag_fmt", name))
+      return modules[name] and modules[name].helptag or error(string.format("unknown module %s passed to helptag_fmt", name))
     end,
+
+    -- class/function's help tag
     fn_helptag_fmt = function(fun)
       -- Modified copy of fn_helptag_fmt_common
       -- Uses fully qualified class name in the tag for methods.
