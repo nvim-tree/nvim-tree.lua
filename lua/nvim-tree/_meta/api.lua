6faff066119739f5bb6af009d9f88fa297139765
Alexander Courtis <alex@courtis.org>
2026-01-06 15:39:05 +1100

    doc(#2934): remove api/decorator as they are out of scope, revert api_decorator.lua changes, retain api opts classes but make them exact

--- lua/nvim-tree/_meta/api.lua
+++ lua/nvim-tree/_meta/api.lua
@@ -5,7 +5,7 @@ error("Cannot require a meta file")
 -- API Options
 --
 
----@class nvim_tree.api.TreeOpenOpts
+---@class (exact) nvim_tree.api.TreeOpenOpts
 ---@field path? string root directory for the tree
 ---@field current_window? boolean open the tree in the current window
 ---@field winid? number open the tree in the specified winid, overrides current_window
@@ -13,7 +13,7 @@ error("Cannot require a meta file")
 ---@field update_root? boolean requires find_file, see |nvim-tree.update_focused_file.update_root|
 ---@field focus? boolean focus the tree when opening, default true
 
----@class nvim_tree.api.TreeToggleOpts
+---@class (exact) nvim_tree.api.TreeToggleOpts
 ---@field path? string root directory for the tree
 ---@field current_window? boolean open the tree in the current window
 ---@field winid? number open the tree in the specified |winid|, overrides current_window
@@ -21,12 +21,12 @@ error("Cannot require a meta file")
 ---@field update_root? boolean requires find_file, see |nvim-tree.update_focused_file.update_root|
 ---@field focus? boolean focus the tree when opening, default true
 
----@class nvim_tree.api.TreeResizeOpts
+---@class (exact) nvim_tree.api.TreeResizeOpts
 ---@field width? string|function|number|table new |nvim-tree.view.width| value
 ---@field absolute? number set the width
 ---@field relative? number relative width adjustment
 
----@class nvim_tree.api.TreeFindFileOpts
+---@class (exact) nvim_tree.api.TreeFindFileOpts
 ---@field buf? string|number absolute/relative path OR bufnr to find
 ---@field open? boolean open the tree if necessary
 ---@field current_window? boolean requires open, open in the current window
@@ -34,24 +34,24 @@ error("Cannot require a meta file")
 ---@field update_root? boolean see |nvim-tree.update_focused_file.update_root|
 ---@field focus? boolean focus the tree
 
----@class nvim_tree.api.CollapseOpts
+---@class (exact) nvim_tree.api.CollapseOpts
 ---@field keep_buffers? boolean do not collapse nodes with open buffers
 
----@class nvim_tree.api.TreeExpandOpts
+---@class (exact) nvim_tree.api.TreeExpandOpts
 ---@field expand_until? (fun(expansion_count: integer, node: Node): boolean) Return true if node should be expanded. expansion_count is the total number of folders expanded.
 
----@class nvim_tree.api.TreeIsVisibleOpts
+---@class (exact) nvim_tree.api.TreeIsVisibleOpts
 ---@field tabpage? number as per |nvim_get_current_tabpage()|
 ---@field any_tabpage? boolean visible on any tab, default false
 
----@class nvim_tree.api.TreeWinIdOpts
+---@class (exact) nvim_tree.api.TreeWinIdOpts
 ---@field tabpage? number tabpage, 0 or nil for current, default nil
 
----@class nvim_tree.api.NodeEditOpts
+---@class (exact) nvim_tree.api.NodeEditOpts
 ---@field quit_on_open? boolean quits the tree when opening the file
 ---@field focus? boolean keep focus in the tree when opening the file
 
----@class nvim_tree.api.NodeBufferOpts
+---@class (exact) nvim_tree.api.NodeBufferOpts
 ---@field force? boolean delete/wipe even if buffer is modified, default false
 
 --
--- lua/nvim-tree/_meta/api_decorator.lua
+++ lua/nvim-tree/_meta/api_decorator.lua
@@ -1,7 +1,7 @@
 ---@meta
 error("Cannot require a meta file")
 
--- TODO #2934 add enum docs
+local nvim_tree = { api = { decorator = {} } }
 
 ---Highlight group range as per nvim-tree.renderer.highlight_*
 ---@alias nvim_tree.api.decorator.HighlightRange "none" | "icon" | "name" | "all"
@@ -14,41 +14,41 @@ error("Cannot require a meta file")
 
 ---Custom decorator, see :help nvim-tree-decorators
 ---
----@class nvim_tree.api.decorator.UserDecorator
----@field enabled boolean
----@field highlight_range nvim_tree.api.decorator.HighlightRange
----@field icon_placement nvim_tree.api.decorator.IconPlacement
-local UserDecorator = {}
+---@class (exact) nvim_tree.api.decorator.UserDecorator
+---@field protected enabled boolean
+---@field protected highlight_range nvim_tree.api.decorator.HighlightRange
+---@field protected icon_placement nvim_tree.api.decorator.IconPlacement
+nvim_tree.api.decorator.UserDecorator = {}
 
 ---Create your decorator class
 ---
-function UserDecorator:extend() end
+function nvim_tree.api.decorator.UserDecorator:extend() end
 
 ---Abstract: no-args constructor must be implemented and will be called once per tree render.
 ---Must set all fields.
 ---
-function UserDecorator:new() end
+function nvim_tree.api.decorator.UserDecorator:new() end
 
 ---Abstract: optionally implement to set the node's icon
 ---
 ---@param node nvim_tree.api.Node
 ---@return nvim_tree.api.HighlightedString? icon_node
-function UserDecorator:icon_node(node) end
+function nvim_tree.api.decorator.UserDecorator:icon_node(node) end
 
 ---Abstract: optionally implement to provide icons and the highlight groups for your icon_placement.
 ---
 ---@param node nvim_tree.api.Node
 ---@return nvim_tree.api.HighlightedString[]? icons
-function UserDecorator:icons(node) end
+function nvim_tree.api.decorator.UserDecorator:icons(node) end
 
 ---Abstract: optionally implement to provide one highlight group to apply to your highlight_range.
 ---
 ---@param node nvim_tree.api.Node
 ---@return string? highlight_group
-function UserDecorator:highlight_group(node) end
+function nvim_tree.api.decorator.UserDecorator:highlight_group(node) end
 
 ---Define a sign. This should be called in the constructor.
 ---
 ---@protected
 ---@param icon nvim_tree.api.HighlightedString?
-function UserDecorator:define_sign(icon) end
+function nvim_tree.api.decorator.UserDecorator:define_sign(icon) end
