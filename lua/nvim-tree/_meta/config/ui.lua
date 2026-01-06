b449929f7c4d69ca85a2493212494bf1c60b5958
Alexander Courtis <alex@courtis.org>
2026-01-06 13:51:36 +1100

    doc(#2934): tidy Config.UI, move into place

--- lua/nvim-tree/_meta/config/ui.lua
+++ lua/nvim-tree/_meta/config/ui.lua
@@ -1,5 +1,26 @@
 ---@meta
 error("Cannot require a meta file")
 
---- TODO #2934
+---@class nvim_tree.Config.UI
+---
+---|nvim_tree.Config.UI.Confirm|
+---@field confirm? nvim_tree.Config.UI.Confirm
 
+--
+-- UI.Confirm
+--
+
+---Confirmation prompts.
+---@class nvim_tree.Config.UI.Confirm
+---
+---Prompt before removing.
+---(default: `true`)
+---@field remove? boolean
+---
+---Prompt before trashing.
+---(default: `true`)
+---@field trash? boolean
+---
+---If `true` the prompt will be `Y/n`, otherwise `y/N`
+---(default: `false`)
+---@field default_yes? boolean
