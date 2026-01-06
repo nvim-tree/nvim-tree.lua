b59183a35d7ce023002fb5acdf546fb1b9fa57ca
Alexander Courtis <alex@courtis.org>
2026-01-06 13:28:41 +1100

    doc(#2934): tidy Config.LiveFilter, move into place

--- lua/nvim-tree/_meta/config/live_filter.lua
+++ lua/nvim-tree/_meta/config/live_filter.lua
@@ -1,5 +1,16 @@
 ---@meta
 error("Cannot require a meta file")
 
---- TODO #2934
+--- Live filter allows you to filter the tree nodes dynamically, based on regex matching, see |vim.regex|
+---
+--- This feature is bound to the `f` key by default. The filter can be cleared with the `F` key by default.
+---@class nvim_tree.Config.LiveFilter
+---
+---Prefix of the filter displayed in the buffer.
+---(default: `[FILTER]: `)
+---@field prefix? string
+---
+---Whether to filter folders or not.
+---(default: `true`)
+---@field always_show_folders? boolean
 
