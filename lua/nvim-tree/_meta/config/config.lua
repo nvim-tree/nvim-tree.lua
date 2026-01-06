901c84fdfe3dbb556d890a77b68182a0d84b8046
Alexander Courtis <alex@courtis.org>
2026-01-06 13:26:52 +1100

    doc(#2934): tidy Config.Modified, move into place

--- lua/nvim-tree/_meta/config/config.lua
+++ lua/nvim-tree/_meta/config/config.lua
@@ -228,23 +228,6 @@ error("Cannot require a meta file")
 -- Modified
 --
 
----Indicate which file have unsaved modification.
----To see modified status in the tree you will need to set:
---- - |nvim_tree.Config.Renderer.Icons.Show| `modified` to `true` OR
---- - |nvim_tree.Config.Renderer| `highlight_modified` to `true`
----@class nvim_tree.Config.Modified
----
----Enable modified.
----(default: `false`)
----@field enable? boolean
----
----Show modified indication on directory whose children are modified.
----(default: `true`)
----@field show_on_dirs? boolean
----
----Show modified indication on open directories. Only relevant when `show_on_dirs` is `true`.
----(default: `false`)
----@field show_on_open_dirs? boolean
 
 --
 -- Tab
