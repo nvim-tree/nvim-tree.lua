9dc17adc0f9d51af439fe37fbb12d72d4e1757bc
Alexander Courtis <alex@courtis.org>
2026-01-06 14:00:26 +1100

    doc(#2934): tidy Config.Notify, move into place

--- lua/nvim-tree/_meta/config/notify.lua
+++ lua/nvim-tree/_meta/config/notify.lua
@@ -1,5 +1,18 @@
 ---@meta
 error("Cannot require a meta file")
 
---- TODO #2934
-
+---nvim-tree notifications levels:
+---- ERROR: hard errors e.g. failure to read from the file system.
+---- WARN: non-fatal errors e.g. unable to system open a file.
+---- INFO: information only e.g. file copy path confirmation.
+---- DEBUG: information for troubleshooting, e.g. failures in some window closing operations.
+---
+---@class nvim_tree.Config.Notify
+---
+---Specify minimum notification level
+---(Default: `vim.log.levels.INFO`)
+---@field threshold? vim.log.levels
+---
+---Use absolute paths in FS action notifications, otherwise item names.
+---(default: `true`)
+---@field absolute_path? boolean
