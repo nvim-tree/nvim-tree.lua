9b9263aded03d1160d8d6e3473ea75aadf0d30b2
Alexander Courtis <alex@courtis.org>
2026-01-06 11:53:34 +1100

    doc(#2934): split nvim_tree.Config into files, Sort and HijackDirectories populate others placeholder

--- lua/nvim-tree/_meta/config/config.lua
+++ lua/nvim-tree/_meta/config/config.lua
@@ -127,20 +127,6 @@ error("Cannot require a meta file")
 ---@field log? nvim_tree.Config.Log
 
 --
--- HijackDirectories
---
-
----@class nvim_tree.Config.HijackDirectories
----
----Hijack directory buffers. Disable this option if you use vim-dirvish or dirbuf.nvim. If `hijack_netrw` and `disable_netrw` are `false`, this feature will be disabled.
----(default: `true`)
----@field enable? boolean
----
----Opens the tree if the tree was previously closed.
----(default: `true`)
----@field auto_open? boolean
-
---
 -- Renderer
 --
 
@@ -338,15 +324,6 @@ error("Cannot require a meta file")
 
 ---@class nvim_tree.Config.Help
 ---@field sort_by? nvim_tree.HelpSortOption Defines how mappings are sorted in the help window. Can be `"key"` (sort alphabetically by keymap) or `"desc"` (sort alphabetically by description). Default: `"key"`
-
---
--- Sort
---
-
----@class nvim_tree.Config.Sort
----@field sorter? nvim_tree.SortOption|fun(nodes: table): nil Changes how files within the same directory are sorted. Can be one of `"name"`, `"case_sensitive"`, `"modification_time"`, `"extension"`, `"suffix"`, `"filetype"` or a function. `"extension"` uses all suffixes e.g. `foo.tar.gz` -> `.tar.gz` `"suffix"` uses the last e.g. `.gz` Default: `"name"` Function may perform a sort or return a string with one of the above methods. It is passed a table of nodes to be sorted, each node containing: - `absolute_path`: `string` - `executable`: `boolean` - `extension`: `string` - `filetype`: `string` - `link_to`: `string` - `name`: `string` - `type`: `"directory"` | `"file"` | `"link"`
----@field folders_first? boolean Sort folders before files. Has no effect when |nvim-tree.sort.sorter| is a function. Default: `true` @see nvim-tree.sort.sorter
----@field files_first? boolean Sort files before folders. Has no effect when |nvim-tree.sort.sorter| is a function. If set to `true` it overrides |nvim-tree.sort.folders_first|. Default: `false` @see nvim-tree.sort.sorter @see nvim-tree.sort.folders_first
 
 --
 -- Filters
