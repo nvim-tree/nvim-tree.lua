ea6dde70f987a5259cc3b03ce23c87231c092d8e
Alexander Courtis <alex@courtis.org>
2026-01-06 15:04:44 +1100

    doc(#2934): tidy Config.Help, move into place

--- lua/nvim-tree/_meta/config/help.lua
+++ lua/nvim-tree/_meta/config/help.lua
@@ -5,10 +5,12 @@ error("Cannot require a meta file")
 
 ---Configure help window, default mapping `g?`
 ---
----Valid {sort_by}:
----- `key`: sort alphabetically by keymap
----- `desc`: sort alphabetically by description
+---[nvim_tree.Config.Help.SortBy]()
+---- `key`: alphabetically by keymap
+---- `desc`: alphabetically by description
+---
 ---@class nvim_tree.Config.Help
 ---
+---|nvim_tree.Config.Help.SortBy|
 ---(default: `key`)
 ---@field sort_by? nvim_tree.Config.Help.SortBy
