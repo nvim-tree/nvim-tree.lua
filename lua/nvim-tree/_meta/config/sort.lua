5877d20a6fd41c2578487a824a6da8b5cb280e7e
Alexander Courtis <alex@courtis.org>
2026-01-10 13:34:04 +1100

    doc(#2934): tidy Config.Sort, move into place

--- lua/nvim-tree/_meta/config/sort.lua
+++ lua/nvim-tree/_meta/config/sort.lua
@@ -1,11 +1,42 @@
 ---@meta
 error("Cannot require a meta file")
 
---- TODO #2934
+---@alias nvim_tree.Config.Sort.Sorter "name"|"case_sensitive"|"modification_time"|"extension"|"suffix"|"filetype"
 
----@alias nvim_tree.SortOption "name"|"case_sensitive"|"modification_time"|"extension"|"suffix"|"filetype"
-
+---Sort files within a directory.
+---
+---{sorter} builtin |nvim_tree.Config.Sort.Sorter|:
+---- `name`
+---- `case_sensitive` name
+---- `modification_time`
+---- `extension` uses all suffixes e.g. `foo.tar.gz` -> `.tar.gz`
+---- `suffix` uses the last e.g. `foo.tar.gz` -> `.gz`
+---- `filetype` |filetype|
+---
+---{sorter} may be a function that is passed a list of |nvim_tree.api.Node| to be sorted in place e.g.
+---```lua
+---
+------Sort by name length
+------@param nodes nvim_tree.api.Node[]
+------@return nvim_tree.Config.Sort.Sorter?
+---local sorter = function(nodes)
+---  table.sort(nodes, function(a, b)
+---    return #a.name < #b.name
+---  end)
+---end
+---```
+---Alternatively, the function may return a |nvim_tree.Config.Sort.Sorter|
+---
 ---@class nvim_tree.Config.Sort
----@field sorter? nvim_tree.SortOption|fun(nodes: table): nil Changes how files within the same directory are sorted. Can be one of `"name"`, `"case_sensitive"`, `"modification_time"`, `"extension"`, `"suffix"`, `"filetype"` or a function. `"extension"` uses all suffixes e.g. `foo.tar.gz` -> `.tar.gz` `"suffix"` uses the last e.g. `.gz` Default: `"name"` Function may perform a sort or return a string with one of the above methods. It is passed a table of nodes to be sorted, each node containing: - `absolute_path`: `string` - `executable`: `boolean` - `extension`: `string` - `filetype`: `string` - `link_to`: `string` - `name`: `string` - `type`: `"directory"` | `"file"` | `"link"`
----@field folders_first? boolean Sort folders before files. Has no effect when |nvim-tree.sort.sorter| is a function. Default: `true` @see nvim-tree.sort.sorter
----@field files_first? boolean Sort files before folders. Has no effect when |nvim-tree.sort.sorter| is a function. If set to `true` it overrides |nvim-tree.sort.folders_first|. Default: `false` @see nvim-tree.sort.sorter @see nvim-tree.sort.folders_first
+---
+---(default: `name`)
+---@field sorter? nvim_tree.Config.Sort.Sorter|fun(nodes: nvim_tree.api.Node[]): nvim_tree.Config.Sort.Sorter?
+---
+---Sort folders before files. Has no effect when {sorter} is a function.
+---(default: `true`)
+---@field folders_first? boolean
+---
+---Sort files before folders. Has no effect when {sorter} is a function. Overrides {folders_first}.
+---(default: `false`)
+---@field files_first? boolean
+---
