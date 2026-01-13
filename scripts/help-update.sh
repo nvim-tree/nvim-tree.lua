2c27f24844fa68dea63c677aabc766a19c73974d
Alexander Courtis <alex@courtis.org>
2026-01-13 11:00:55 +1100

    docs(#2934): remove indices

--- scripts/help-update.sh
+++ scripts/help-update.sh
@@ -21,39 +21,6 @@ sed -e "s/^  /      /" /tmp/DEFAULT_OPTS.2.lua > /tmp/DEFAULT_OPTS.6.lua
 sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/DEFAULT_OPTS.6.lua
            }; /${end}/p; d; }" doc/nvim-tree-lua.txt
 
-
-#
-# opts index
-#
-begin="nvim-tree-index-opts\*"
-end="====================="
-
-printf '\n' > /tmp/index-opts.txt
-sed -E "
-/^ *\*(nvim-tree\..*)\*$/! d ;
-s/^.*\*(.*)\*/|\1|/g
-" doc/nvim-tree-lua.txt | sort -d >> /tmp/index-opts.txt
-printf '\n' >> /tmp/index-opts.txt
-
-sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/index-opts.txt
-           }; /${end}/p; d; }" doc/nvim-tree-lua.txt
-
-#
-# api index
-#
-begin="nvim-tree-index-api\*"
-end="====================="
-
-printf '\n' > /tmp/index-api.txt
-sed -E "
-/\*(nvim-tree-api.*\(\))\*/! d ;
-s/^.*\*(.*)\*/|\1|/g
-" doc/nvim-tree-lua.txt | sort -d >> /tmp/index-api.txt
-printf '\n' >> /tmp/index-api.txt
-
-sed -i -e "/${begin}/,/${end}/{ /${begin}/{p; r /tmp/index-api.txt
-           }; /${end}/p; d; }" doc/nvim-tree-lua.txt
-
 #
 # DEFAULT_ON_ATTACH
 #
