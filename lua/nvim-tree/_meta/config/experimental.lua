20b2be17bf8116a6a0b2c053254caeaca6313b7b
Alexander Courtis <alex@courtis.org>
2026-01-15 10:55:23 +1100

    docs(#2934): add experimental example

--- lua/nvim-tree/_meta/config/experimental.lua
+++ lua/nvim-tree/_meta/config/experimental.lua
@@ -8,3 +8,10 @@ error("Cannot require a meta file")
 ---In the event of a problem please disable the experiment and raise an issue.
 ---
 ---@class nvim_tree.Config.Experimental
+---
+--Example below for future reference:
+--
+--Buffers opened by nvim-tree will use with relative paths instead of absolute.
+--(default: false)
+--@field relative_path? boolean
+
