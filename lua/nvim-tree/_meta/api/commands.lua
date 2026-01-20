4f966c687bab4119eaf73383777093d3c80da52b
Alexander Courtis <alex@courtis.org>
2026-01-20 15:31:12 +1100

    docs(#3088): format nvim_tree.api.commands

--- lua/nvim-tree/_meta/api/commands.lua
+++ lua/nvim-tree/_meta/api/commands.lua
@@ -1,17 +1,19 @@
 ---@meta
 local nvim_tree = { api = { commands = {} } }
 
----
----Arguments for [nvim_create_user_command()]
----
----@class nvim_tree.api.commands.Command
----
----@field name string
----@field command fun(args: vim.api.keyset.create_user_command.command_args)
----@field opts vim.api.keyset.user_command
 
 ---
----Retrieve all nvim-tree commands, see [nvim-tree-commands]
+---@class nvim_tree.api.commands.Command
+---@inlinedoc
+---
+---@field name string name of the `:NvimTree*` command
+---@field command fun(args: vim.api.keyset.create_user_command.command_args) function that the command will execute
+---@field opts vim.api.keyset.user_command [command-attributes]
+
+---
+---Retrieve all [nvim-tree-commands]
+---
+---They have been created via [nvim_create_user_command()], see also [lua-guide-commands-create]
 ---
 ---@return nvim_tree.api.commands.Command[]
 function nvim_tree.api.commands.get() end
