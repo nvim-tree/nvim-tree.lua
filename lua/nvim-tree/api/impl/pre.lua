c6ea6c5be1aab9792fa45e1a749e6bc90d58a6bc
Alexander Courtis <alex@courtis.org>
2026-01-24 15:39:49 +1100

    docs(#3088): lazy api impl requires

--- lua/nvim-tree/api/impl/pre.lua
+++ lua/nvim-tree/api/impl/pre.lua
@@ -1,3 +1,6 @@
+--This file should have no requires barring the essentials.
+--Everything must be as lazily loaded as possible.
+--The user must be able to require api cheaply and run setup cheaply.
 local events = require("nvim-tree.events")
 local keymap = require("nvim-tree.keymap")
 local notify = require("nvim-tree.notify")
@@ -6,14 +9,14 @@ local UserDecorator = require("nvim-tree.renderer.decorator.user")
 
 ---Walk the api, hydrating all functions with the error notification
 ---@param t table api root or sub-module
-local function hydrate_notify(t)
+local function hydrate_error(t)
   for k, v in pairs(t) do
     if type(v) == "function" then
       t[k] = function()
         notify.error("nvim-tree setup not called")
       end
     elseif type(v) == "table" then
-      hydrate_notify(v)
+      hydrate_error(v)
     end
   end
 end
@@ -21,8 +24,18 @@ end
 ---Hydrate implementations that may be called pre setup
 ---@param api table
 local function hydrate_pre(api)
-  api.events.subscribe = events.subscribe
+  --
+  -- May be lazily requried on execution
+  --
+  api.commands.get = function() require("nvim-tree.commands").get() end
+
+  api.health.hi_test = function() require("nvim-tree.appearance.hi-test")() end
+
+  --
+  -- Must be eagerly required or common
+  --
   api.events.Event = events.Event
+  api.events.subscribe = events.subscribe
 
   api.map.default_on_attach = keymap.default_on_attach
 
@@ -36,15 +49,11 @@ end
 --Hydrates meta api empty definition functions with a new function:
 -- - Default: error notification "nvim-tree setup not called".
 -- - Exceptions: concrete implementation for API that can be called before setup.
---
 --Call it once when api is first required
---
---This should not include any requires beyond that which is absolutely essential,
---as the user should be able to require api cheaply.
 ---@param api table
 return function(api)
   -- Default: error
-  hydrate_notify(api)
+  hydrate_error(api)
 
   -- Exceptions: may be called
   hydrate_pre(api)
