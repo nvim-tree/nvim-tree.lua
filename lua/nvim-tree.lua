7ec141600dbb4b3fbf04c5437c52134f6f83ea79
Alexander Courtis <alex@courtis.org>
2026-02-13 13:22:47 +1100

    refactor(#3253): add api.config, tidy new config module

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -275,9 +275,6 @@ function M.purge_all_state()
 end
 
 
--- TODO #3253 ensure that we can call setup twice
-
-
 ---@param config_user? nvim_tree.config user supplied subset of config
 function M.setup(config_user)
   if vim.fn.has("nvim-0.9") == 0 then
