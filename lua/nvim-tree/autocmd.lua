b117111cc24c4ef98b3617bb0f8d8acf3d1eb594
Igor <igorlfs@ufmg.br>
2026-06-12 00:22:19 -0300

    feat: restore buffers after loading a session

--- lua/nvim-tree/autocmd.lua
+++ lua/nvim-tree/autocmd.lua
@@ -17,6 +17,41 @@ function M.global()
     end,
   })
 
+  vim.api.nvim_create_autocmd("SessionLoadPost", {
+    group = augroup_id,
+    callback = function()
+      local api = require("nvim-tree.api").tree
+
+      ---@type table<integer,boolean>
+      local tabs = {}
+
+      for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if api.is_tree_buf(buf) then
+            tabs[tab] = true
+          end
+        end
+      end
+
+      vim.schedule(function()
+        api.close_in_all_tabs()
+
+        for tab, _ in pairs(tabs) do
+          if vim.api.nvim_tabpage_is_valid(tab) then
+            local win = vim.api.nvim_tabpage_get_win(tab)
+
+            if vim.api.nvim_win_is_valid(win) then
+              vim.api.nvim_win_call(win, function()
+                api.open()
+              end)
+            end
+          end
+        end
+      end)
+    end,
+  })
+
   if config.g.tab.sync.open then
     vim.api.nvim_create_autocmd("TabEnter", {
       group = augroup_id,
