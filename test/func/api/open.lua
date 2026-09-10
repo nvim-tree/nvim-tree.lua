db732e6c2b0003e57d9b8cdea504286377daddc4
Alexander Courtis <alex@courtis.org>
2026-09-10 12:58:13 +1000

    test: refactor functional test lifecycle, add another test

--- test/func/api/open.lua
+++ test/func/api/open.lua
@@ -2,8 +2,51 @@ local t = require("test.testutil")
 local n = require("test.functional.testnvim")()
 local Screen = require("test.functional.ui.screen")
 local clear = n.clear
-local command = n.command
 local exec_lua = n.exec_lua
+
+local function nvt_hl_attr_ids(screen)
+  local attr_ids = {}
+
+  -- math.random(tonumber('0x707070'),tonumber('0x909090'))
+  local bg_rgb, bg_hex = 8758352, "#85a450"
+
+  -- build the cursor line first, background only
+  attr_ids["NvimTreeCursorLine"] = { background = bg_rgb }
+  n.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = bg_hex })
+
+  -- unique colour for each group, descending from #fefefe
+  local i, r, g, b = 1, 254, 254, 254
+  local rgb, hex
+
+  for group, _ in pairs(n.api.nvim_get_hl(0, { create = false })) do
+    if group ~= "NvimTreeCursorLine" and group:match("^NvimTree.*") then
+      -- next unique fg colour
+      rgb = r * 256 * 256 + g * 256 + b
+      hex = string.format("#%x", rgb)
+      i = i + 1
+      if (i % 256 == 0) then
+        b = 254
+        g = 254
+        r = r - 1
+      elseif (i % 16 == 0) then
+        b = 254
+        g = g - 1
+      else
+        b = b - 1
+      end
+
+      -- add the group's concrete definition with fg only
+      attr_ids[group] = { foreground = rgb }
+      n.api.nvim_set_hl(0, group, { fg = hex })
+
+      -- create an NvimTreeCursorLine variant
+      attr_ids[group .. "CL"] = { foreground = rgb, background = bg_rgb }
+      n.api.nvim_set_hl(0, group .. "CL", { fg = hex, bg = bg_hex })
+    end
+  end
+
+  screen:add_extra_attr_ids(attr_ids)
+end
 
 describe("api_open", function()
   --- @type test.functional.ui.screen
@@ -13,9 +56,20 @@ describe("api_open", function()
     -- TODO this can be done without -u and a direct vim.pack.add
     clear({ args_rm = { "-u" } })
 
-    command("packadd nvim-tree.lua")
+    n.api.nvim_cmd({ cmd = "packadd", args = { "nvim-tree.lua" } }, {})
 
     screen = Screen.new(40, 20)
+
+    exec_lua(function()
+      require("nvim-tree").setup({
+        renderer = {
+          root_folder_label = false,
+        },
+      })
+    end)
+
+    -- TODO setting all hl groups causes expect to take much longer to execute
+    nvt_hl_attr_ids(screen)
   end)
 
   before_each(function()
@@ -27,72 +81,14 @@ describe("api_open", function()
     assert(t.mkdir(tmp .. "/dir1"))
     assert(t.mkdir(tmp .. "/dir2"))
 
-    command(":cd " .. tmp)
-
-    -- TODO try and use a function, not a lua string, if we have luals issues
-    exec_lua([[
-      require("nvim-tree").setup({
-        renderer = {
-          root_folder_label = false,
-        },
-      })
-      ]])
+    n.api.nvim_cmd({ cmd = "cd", args = { tmp } }, {})
   end)
 
-  local function nvt_hl_attr_ids()
-    local attr_ids = {}
-
-    -- math.random(tonumber('0x707070'),tonumber('0x909090'))
-    local bg_rgb, bg_hex = 8758352, "#85a450"
-
-    -- build the cursor line first, background only
-    attr_ids["NvimTreeCursorLine"] = { background = bg_rgb }
-    n.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = bg_hex })
-
-    -- unique colour for each group, descending from #fefefe
-    local i, r, g, b = 1, 254, 254, 254
-    local rgb, hex
-
-    for group, _ in pairs(n.api.nvim_get_hl(0, { create = false })) do
-      if group ~= "NvimTreeCursorLine" and group:match("^NvimTree.*") then
-
-        -- next unique fg colour
-        rgb = r * 256 * 256 + g * 256 + b
-        hex = string.format("#%x", rgb)
-        i = i + 1
-        if (i % 256 == 0) then
-          b = 254
-          g = 254
-          r = r - 1
-        elseif (i % 16 == 0) then
-          b = 254
-          g = g - 1
-        else
-          b = b - 1
-        end
-
-        -- add the group's concrete definition with fg only
-        attr_ids[group] = { foreground = rgb }
-        n.api.nvim_set_hl(0, group, { fg = hex })
-
-        -- create an NvimTreeCursorLine variant
-        attr_ids[group .. "CL"] = { foreground = rgb, background = bg_rgb }
-        n.api.nvim_set_hl(0, group .. "CL", { fg = hex, bg = bg_hex })
-      end
-    end
-
-    return attr_ids
-  end
-
-  it("api_tree_open_populated", function()
-    exec_lua([[
+  it("api_tree_open_populated_focussed", function()
+    exec_lua(function()
       local api = require("nvim-tree.api")
       api.tree.open()
-      ]])
-
-    -- TODO setting all hl groups causes expect to take much longer to execute
-    local attr_ids = nvt_hl_attr_ids()
-    screen:add_extra_attr_ids(attr_ids)
+    end)
 
     -- TODO NvimTreeFolderArrowClosed is always set by Padding:get_arrows, it should only be set for DirectoryNode
     screen:expect({
@@ -103,6 +99,28 @@ describe("api_open", function()
       {NvimTreeSignColumn:  }{NvimTreeFolderArrowClosed:  }{NvimTreeFileIcon:}{NvimTreeNormal: file2                   }{NvimTreeWinSeparator:│}{1:~        }|
       {NvimTreeEndOfBuffer:~                             }{NvimTreeWinSeparator:│}{1:~        }|*14
       {NvimTreeStatusLine:NvimTree_1 [-]                 }{2:<o Name] }|
+                                              |
+    ]],
+    })
+  end)
+
+  it("api_tree_open_populated_unfocussed", function()
+    -- TODO consider resetting tree after each test, they are order dependent
+    -- exec_lua(function()
+    --   local api = require("nvim-tree.api")
+    --   api.tree.open()
+    -- end)
+
+    n.feed("<c-w><c-w>")
+
+    screen:expect({
+      grid = [[
+      {NvimTreeSignColumn:  }{NvimTreeFolderArrowClosedCL: }{NvimTreeClosedFolderIconCL:}{NvimTreeNormalNCCL: }{NvimTreeEmptyFolderNameCL:dir1}{NvimTreeNormalNCCL:         }{NvimTreeWinSeparator:│}^                    |
+      {NvimTreeSignColumn:  }{NvimTreeFolderArrowClosed: }{NvimTreeClosedFolderIcon:}{NvimTreeNormalNC: }{NvimTreeEmptyFolderName:dir2}{NvimTreeNormalNC:         }{NvimTreeWinSeparator:│}{1:~                   }|
+      {NvimTreeSignColumn:  }{NvimTreeFolderArrowClosed:  }{NvimTreeFileIcon:}{NvimTreeNormalNC: file1        }{NvimTreeWinSeparator:│}{1:~                   }|
+      {NvimTreeSignColumn:  }{NvimTreeFolderArrowClosed:  }{NvimTreeFileIcon:}{NvimTreeNormalNC: file2        }{NvimTreeWinSeparator:│}{1:~                   }|
+      {NvimTreeEndOfBuffer:~                  }{NvimTreeWinSeparator:│}{1:~                   }|*14
+      {NvimTreeStatusLineNC:NvimTree_1 [-]      }{3:[No Name]           }|
                                               |
     ]],
     })
